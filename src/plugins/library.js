4726882daac07774e8e0f74dac7c8661662fd313
Forrest Oliphant <forrest@sembiki.com>
2012-10-26 17:41:57 -0400

    library exclude

--- src/plugins/library.js
+++ src/plugins/library.js
@@ -2,26 +2,33 @@
  
   var library = $("<ul />");
 
+  // function addNodeToCurrent(node) {
+  // }
+
+  var exclude = ["base", "base-resizable"];
+
   _.each(Dataflow.nodes, function(node, index){
-    var addButton = $("<button>+</button>")
-      .click(function(){
-        // Find vacant id
-        var id = 1;
-        while (Dataflow.currentGraph.nodes.get(id)){
-          id++;
-        }
-        var newNode = new node.Model({
-          id: id,
-          x: window.scrollX - 100 + Math.floor($(window).width()/2),
-          y: window.scrollY - 100 + Math.floor($(window).height()/2),
-          parentGraph: Dataflow.currentGraph
+    if (exclude.indexOf(index) === -1) {
+      var addButton = $("<button>+</button>")
+        .click(function(){
+          // Find vacant id
+          var id = 1; 
+          while (Dataflow.currentGraph.nodes.get(id)){
+            id++;
+          }
+          var newNode = new node.Model({
+            id: id,
+            x: window.scrollX - 100 + Math.floor($(window).width()/2),
+            y: window.scrollY - 100 + Math.floor($(window).height()/2),
+            parentGraph: Dataflow.currentGraph
+          });
+          Dataflow.currentGraph.nodes.add(newNode);
         });
-        Dataflow.currentGraph.nodes.add(newNode);
-      });
-    var item = $("<li />")
-      .append(addButton)
-      .append(index);
-    library.append(item);
+      var item = $("<li />")
+        .append(addButton)
+        .append(index);
+      library.append(item);
+    }
   });
 
   Dataflow.addPlugin("library", library);
