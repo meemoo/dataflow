c498c623aaab8bcc76232a4598120d5bf2d157f8
Forrest Oliphant <forrest@sembiki.com>
2013-07-28 15:10:34 -0700

    resizable min

--- src/nodes/base-resizable.js
+++ src/nodes/base-resizable.js
@@ -40,8 +40,8 @@
       var self = this;
       this.$el.resizable({
         helper: "dataflow-node helper",
-        minHeight: 75,
-        minWidth: 75,
+        minHeight: 100,
+        minWidth: 120,
         stop: function(event, ui) {
           self.resizeStop(event, ui);
         }
