6a8bb031913faabc7b78147f94ec1328c9dbaee0
Forrest Oliphant <forrest@sembiki.com>
2013-08-21 20:07:04 -0400

    uistub #42

---
+++ libs/jquery-uistub.js
@@ -0,0 +1,15 @@
+(function ( $ ) {
+  
+  $.fn.draggable = function() {
+    return this;
+  };
+
+  $.fn.droppable = function() {
+    return this;
+  };
+
+  $.fn.resizable = function() {
+    return this;
+  };
+ 
+}( jQuery ));
