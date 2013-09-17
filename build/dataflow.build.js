4445f19f1c2b709e68f86afb22d373c10e78b7e3
Forrest Oliphant <forrest@sembiki.com>
2013-09-17 18:20:41 +0200

    actionbar icons smaller to fit

--- build/dataflow.build.js
+++ build/dataflow.build.js
@@ -1,4 +1,4 @@
-/*! dataflow.js - v0.0.7 - 2013-09-17 (4:39:34 PM GMT+0200)
+/*! dataflow.js - v0.0.7 - 2013-09-17 (6:20:20 PM GMT+0200)
 * Copyright (c) 2013 Forrest Oliphant; Licensed MIT, GPL */
 (function(Backbone) {
   var ensure = function (obj, key, type) {
